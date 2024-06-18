26bcd7c2e7117077a1f174c87346349e6a5e719a
chen278947895 <chen278947895@163.com>
2024-06-18 15:50:18 +0800

    add explanation

--- tasks/qa_feedback/reward_modeling/create_rm_train_files.sh
+++ tasks/qa_feedback/reward_modeling/create_rm_train_files.sh
@@ -1,3 +1,6 @@
+####对原始qa文件做sequence、token粒度拆解，并对token粒度做label标记
+#输入文件/home/chenzhengzong/FineGrainedRLHF/tasks/qa_feedback/data/train_feedback.json
+#关注1、question；2、"prediction 1"、3、feedback->errors(span为字符串索引粒度)
 set -e
 
 python tasks/qa_feedback/reward_modeling/create_rel_fact_rm_files.py --feedback_level subsentence --error_category NF-ERR --ignore_context --data_dir ./tasks/qa_feedback/data/
